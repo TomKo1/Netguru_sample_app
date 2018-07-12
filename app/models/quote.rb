f3ef94d1c47bc44b03663246540397e8a65b7483
Tomasz Kot <tomek@imac-tomasz.home>
2018-07-12 22:15:54 +0200

    Added models tests

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,3 @@
 class Quote < ActiveRecord::Base 
     validates :quotation, presence: true, length: { in: 5..255 }
-
 end
---
+++ spec/model/quote_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Quote, type: :model do 
+
+    describe 'validations' do 
+        it { should validate_presence_of(:quotation) }
+        it { should validate_length_of(:quotation).is_at_least(5).is_at_most(255) } 
+    end
+end
