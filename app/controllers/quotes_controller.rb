4062549484295c43844a17bc37aeef9e834b3de7
Tomasz Kot <tomek@imac-tomasz.home>
2018-07-12 21:28:19 +0200

    Fully working app before testing

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,7 +1,8 @@
 class QuotesController < ApplicationController
 
     def index
-        @quotes = Quote.all
+        <!-- some sort of pagination ? -->
+        @quotes = Quote.order(created_at: :desc)
     end
 
     def new 
@@ -9,6 +10,19 @@ class QuotesController < ApplicationController
     end
 
     def create 
-    
+        @quote = Quote.new(quote_params)
+        
+        if @quote.save 
+            redirect_to root_path, notice: 'Quote created!'
+        else
+            render :new
+        end
     end
+
+    private
+        # params that are allowed for security
+        def quote_params
+            params.require(:quote).permit(:quotation)
+        end
+
 end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,4 @@
 class Quote < ActiveRecord::Base 
     validates :quotation, presence: true, length: { in: 5..255 }
 
-
 end
